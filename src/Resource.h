2935a804795c0f5d9628046b61eef4e521da94cc
lif <40145564+lif1234567892@users.noreply.github.com>
2026-09-11 20:49:21 +0800

    Add Resource.h with dialog and control IDs

---
+++ src/Resource.h
@@ -0,0 +1,25 @@
+//
+#define IDD_MFC_TEST_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_EDIT3                       1002
+#define IDC_B_SelLibDir                 1003
+#define IDC_B_SelFile                   1004
+#define IDC_B_SelDir                    1005
+#define IDC_B_CID                       1006
+#define IDC_EDIT4                       1007
+#define IDC_EDIT5                       1012
+#define IDC_BUTTON1                     1021
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
